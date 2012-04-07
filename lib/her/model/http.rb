7770910ada98a91219e9c76323d35a4c41d56e9f
Rémi Prévost <remi@exomel.com>
2012-04-07 08:48:47 -0400

    Model.uses_api belongs in HTTP module

--- lib/her/model/http.rb
+++ lib/her/model/http.rb
@@ -2,7 +2,13 @@ module Her
   module Model
     # This module interacts with Her::API to fetch HTTP data
     module HTTP
-      # Main wrapper around Her::API
+      # Link a model with a Her::API object
+      def uses_api(api) # {{{
+        @her_api = api
+      end # }}}
+
+      # Main request wrapper around Her::API. Used to make custom request to the API.
+      #
       # @private
       def request(attrs={}) # {{{
         p "relationships are"
