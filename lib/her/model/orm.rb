85ac5a1aee0ec14ecf11231889fe2825ca08e5eb
Rémi Prévost <remi@exomel.com>
2012-04-08 23:06:05 -0400

    Reverse merge order

--- lib/her/model/orm.rb
+++ lib/her/model/orm.rb
@@ -24,14 +24,14 @@ module Her
 
       # Fetch a specific resource based on an ID
       def find(id) # {{{
-        request({:_method => :get, :_path => "#{@her_collection_path}/#{id}"}.merge(params)) do |parsed_data|
+        request(params.merge(:_method => :get, :_path => "#{@her_collection_path}/#{id}")) do |parsed_data|
           new(parsed_data[:resource])
         end
       end # }}}
 
       # Fetch a collection of resources
       def all(params={}) # {{{
-        request({ :_method => :get, :_path => "#{@her_collection_path}" }.merge(params)) do |parsed_data|
+        request(params.merge(:_method => :get, :_path => "#{@her_collection_path}")) do |parsed_data|
           Her::Model::ORM.initialize_collection(to_s.downcase.pluralize, parsed_data[:resource])
         end
       end # }}}
