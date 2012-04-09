18c40d09ab8d3deb149dd41653d03b7be20719bb
Rémi Prévost <remi@exomel.com>
2012-04-08 21:17:56 -0400

    Add tests for Her::Model

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -2,21 +2,37 @@
 require File.join(File.dirname(__FILE__), "spec_helper.rb")
 
 describe Her::Model do
-  context Her::Model::HTTP do
-    describe "fetching data" do # {{{
-    end # }}}
+  describe Her::Model::ORM do
+    context "mapping data to Ruby objects" do # {{{
+      before do # {{{
+        @api = Her::API.new
+        @api.setup :base_uri => "https://api.example.com"
+        FakeWeb.register_uri(:get, "https://api.example.com/users/1", :body => { :data => { :id => 1, :name => "Tobias Fünke" } }.to_json)
+        FakeWeb.register_uri(:get, "https://api.example.com/users", :body => { :data => [{ :id => 1, :name => "Tobias Fünke" }, { :id => 2, :name => "Lindsay Fünke" }] }.to_json)
 
-    describe "parsing data" do # {{{
+        User = Class.new
+        class User
+          include Her::Model
+        end
+        User.uses_api @api
+      end # }}}
+
+      it "maps a single resource to a Ruby object" do # {{{
+        @user = User.find(1)
+        @user.id.should == 1
+        @user.name.should == "Tobias Fünke"
+      end # }}}
+
+      it "maps a collection of resources to an array of Ruby objects" do # {{{
+        @users = User.all
+        @users.length.should == 2
+        @users.first.name.should == "Tobias Fünke"
+      end # }}}
     end # }}}
   end
 
-  context Her::Model::ORM do
-    describe "mapping data to Ruby objects" do # {{{
-    end # }}}
-  end
-
-  context Her::Model::Relationships do
-    describe "setting associations" do # {{{
+  describe Her::Model::Relationships do
+    context "setting associations" do # {{{
       before do # {{{
         User = Class.new # FIXME There has to be a better way to reset the class…
         class User
