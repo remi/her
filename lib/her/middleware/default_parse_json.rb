e77f8d67956fc8604e0614859cf7f3e22a2c29df
Rafael Souza <me@rafaelss.com>
2012-04-19 19:37:21 -0300

    using multi_json to parse json so people can choose what coder use, instead force them to use the json one

--- lib/her/middleware/default_parse_json.rb
+++ lib/her/middleware/default_parse_json.rb
@@ -2,16 +2,16 @@ module Her
   module Middleware
     class DefaultParseJSON < Faraday::Response::Middleware
       def parse(body)
-        json = JSON.parse(body, :symbolize_names => true)
-        return {
+        json = MultiJson.load(body, :symbolize_keys => true)
+        {
           :data => json[:data],
           :errors => json[:errors],
-          :metadata => json[:metadata],
+          :metadata => json[:metadata]
         }
       end
 
       def on_complete(env)
-        env[:body] = parse env[:body]
+        env[:body] = parse(env[:body])
       end
     end
   end
