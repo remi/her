54c008a3635cb5515c0d2da04e03d42c91e1d96c
Tyson Tate <tyson@tysontate.com>
2012-05-01 14:44:30 -0700

    Better class name detection for paths.
    Includes spec.

--- spec/model/paths_spec.rb
+++ spec/model/paths_spec.rb
@@ -3,38 +3,53 @@ require File.join(File.dirname(__FILE__), "../spec_helper.rb")
 
 describe Her::Model::Paths do
   context "building request paths" do
-    before do # {{{
-      spawn_model :User
-    end # }}}
-
-    describe "#build_request_path" do
-      it "builds paths with defaults" do # {{{
-        User.build_request_path(id: "foo").should == "/users/foo"
-        User.build_request_path.should == "/users"
+    context "simple model" do
+      before do # {{{
+        spawn_model :User
       end # }}}
 
-      it "builds paths with custom collection path" do # {{{
-        User.collection_path "/utilisateurs"
-        User.build_request_path(id: "foo").should == "/utilisateurs/foo"
-        User.build_request_path.should == "/utilisateurs"
+      describe "#build_request_path" do
+        it "builds paths with defaults" do # {{{
+          User.build_request_path(id: "foo").should == "/users/foo"
+          User.build_request_path.should == "/users"
+        end # }}}
+
+        it "builds paths with custom collection path" do # {{{
+          User.collection_path "/utilisateurs"
+          User.build_request_path(id: "foo").should == "/utilisateurs/foo"
+          User.build_request_path.should == "/utilisateurs"
+        end # }}}
+
+        it "builds paths with custom collection path with multiple variables" do # {{{
+          User.collection_path "/organizations/:organization_id/utilisateurs"
+          User.build_request_path(:id => "foo", :_organization_id => "acme").should == "/organizations/acme/utilisateurs/foo"
+          User.build_request_path(:_organization_id => "acme").should == "/organizations/acme/utilisateurs"
+        end # }}}
+
+        it "builds paths with custom item path" do # {{{
+          User.resource_path "/utilisateurs/:id"
+          User.build_request_path(id: "foo").should == "/utilisateurs/foo"
+          User.build_request_path.should == "/users"
+        end # }}}
+
+        it "raises exceptions when building a path without required custom variables" do # {{{
+          User.collection_path "/organizations/:organization_id/utilisateurs"
+          expect { User.build_request_path(:id => "foo") }.should raise_error(Her::Errors::PathError)
+        end # }}}
+      end
+    end
+
+    context "nested model" do
+      before do # {{{
+        spawn_submodel :Base, :User
       end # }}}
 
-      it "builds paths with custom collection path with multiple variables" do # {{{
-        User.collection_path "/organizations/:organization_id/utilisateurs"
-        User.build_request_path(:id => "foo", :_organization_id => "acme").should == "/organizations/acme/utilisateurs/foo"
-        User.build_request_path(:_organization_id => "acme").should == "/organizations/acme/utilisateurs"
-      end # }}}
-
-      it "builds paths with custom item path" do # {{{
-        User.resource_path "/utilisateurs/:id"
-        User.build_request_path(id: "foo").should == "/utilisateurs/foo"
-        User.build_request_path.should == "/users"
-      end # }}}
-
-      it "raises exceptions when building a path without required custom variables" do # {{{
-        User.collection_path "/organizations/:organization_id/utilisateurs"
-        expect { User.build_request_path(:id => "foo") }.should raise_error(Her::Errors::PathError)
-      end # }}}
+      describe "#build_request_path" do
+        it "builds paths with defaults" do # {{{
+          Base::User.build_request_path(id: "foo").should == "/users/foo"
+          Base::User.build_request_path.should == "/users"
+        end # }}}
+      end
     end
   end
 
