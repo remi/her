d348ab0d81b9dc92bfa46bfbbb964228d615e6df
Tyson Tate <tyson@tysontate.com>
2012-05-05 18:58:21 -0700

    Use submodels in orm_spec.rb

--- spec/model/orm_spec.rb
+++ spec/model/orm_spec.rb
@@ -15,36 +15,36 @@ describe Her::Model::ORM do
       FakeWeb.register_uri(:get, "https://api.example.com/users", :body => [{ :id => 1, :name => "Tobias Fünke" }, { :id => 2, :name => "Lindsay Fünke" }].to_json)
       FakeWeb.register_uri(:get, "https://api.example.com/admin_users", :body => [{ :id => 1, :name => "Tobias Fünke" }, { :id => 2, :name => "Lindsay Fünke" }].to_json)
 
-      spawn_model :User do
+      spawn_model "Foo::User" do
         uses_api api
       end
 
-      spawn_model :AdminUser do
+      spawn_model "Foo::AdminUser" do
         uses_api api
       end
     end # }}}
 
     it "maps a single resource to a Ruby object" do # {{{
-      @user = User.find(1)
+      @user = Foo::User.find(1)
       @user.id.should == 1
       @user.name.should == "Tobias Fünke"
     end # }}}
 
     it "maps a collection of resources to an array of Ruby objects" do # {{{
-      @users = User.all
+      @users = Foo::User.all
       @users.length.should == 2
       @users.first.name.should == "Tobias Fünke"
 
-      @users = AdminUser.all
+      @users = Foo::AdminUser.all
       @users.length.should == 2
       @users.first.name.should == "Tobias Fünke"
     end # }}}
 
     it "handles new resource" do # {{{
-      @new_user = User.new(:fullname => "Tobias Fünke")
+      @new_user = Foo::User.new(:fullname => "Tobias Fünke")
       @new_user.new?.should be_true
 
-      @existing_user = User.find(1)
+      @existing_user = Foo::User.find(1)
       @existing_user.new?.should be_false
     end # }}}
   end
@@ -59,17 +59,17 @@ describe Her::Model::ORM do
 
       FakeWeb.register_uri(:post, "https://api.example.com/users", :body => { :id => 1, :fullname => "Tobias Fünke" }.to_json)
 
-      spawn_model :User
+      spawn_model "Foo::User"
     end # }}}
 
     it "handle one-line resource creation" do # {{{
-      @user = User.create(:fullname => "Tobias Fünke")
+      @user = Foo::User.create(:fullname => "Tobias Fünke")
       @user.id.should == 1
       @user.fullname.should == "Tobias Fünke"
     end # }}}
 
     it "handle resource creation through Model.new + #save" do # {{{
-      @user = User.new(:fullname => "Tobias Fünke")
+      @user = Foo::User.new(:fullname => "Tobias Fünke")
       @user.save
       @user.fullname.should == "Tobias Fünke"
     end # }}}
@@ -87,23 +87,23 @@ describe Her::Model::ORM do
       FakeWeb.register_uri(:get, "https://api.example.com/users/1", :body => { :id => 1, :fullname => "Tobias Fünke" }.to_json)
       FakeWeb.register_uri(:put, "https://api.example.com/users/1", :body => { :id => 1, :fullname => "Lindsay Fünke" }.to_json)
 
-      spawn_model :User
+      spawn_model "Foo::User"
     end # }}}
 
     it "handle resource data update without saving it" do # {{{
-      @user = User.find(1)
+      @user = Foo::User.find(1)
       @user.fullname.should == "Tobias Fünke"
       @user.fullname = "Kittie Sanchez"
       @user.fullname.should == "Kittie Sanchez"
     end # }}}
 
     it "handle resource update through the .update class method" do # {{{
-      @user = User.save_existing(1, { :fullname => "Lindsay Fünke" })
+      @user = Foo::User.save_existing(1, { :fullname => "Lindsay Fünke" })
       @user.fullname.should == "Lindsay Fünke"
     end # }}}
 
     it "handle resource update through #save on an existing resource" do # {{{
-      @user = User.find(1)
+      @user = Foo::User.find(1)
       @user.fullname = "Lindsay Fünke"
       @user.save
       @user.fullname.should == "Lindsay Fünke"
@@ -122,16 +122,16 @@ describe Her::Model::ORM do
       FakeWeb.register_uri(:get, "https://api.example.com/users/1", :body => { :id => 1, :fullname => "Tobias Fünke", :active => true }.to_json)
       FakeWeb.register_uri(:delete, "https://api.example.com/users/1", :body => { :id => 1, :fullname => "Lindsay Fünke", :active => false }.to_json)
 
-      spawn_model :User
+      spawn_model "Foo::User"
     end # }}}
 
     it "handle resource deletion through the .destroy class method" do # {{{
-      @user = User.destroy_existing(1)
+      @user = Foo::User.destroy_existing(1)
       @user.active.should be_false
     end # }}}
 
     it "handle resource deletion through #destroy on an existing resource" do # {{{
-      @user = User.find(1)
+      @user = Foo::User.find(1)
       @user.destroy
       @user.active.should be_false
     end # }}}
