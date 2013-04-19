e8227dd4314712697ecaa39974562ce60129e2a7
Rémi Prévost <remi@exomel.com>
2013-04-19 11:51:56 -0400

    Do not fetch belongs_to association if there are no foreign_key value

--- lib/her/model/associations/belongs_to_association.rb
+++ lib/her/model/associations/belongs_to_association.rb
@@ -11,11 +11,12 @@ module Her
         end
 
         def fetch
-          return nil if @parent.attributes.include?(@name) && @parent.attributes[@name].nil? && @query_attrs.empty?
+          foreign_key_value = @parent.attributes[@opts[:foreign_key].to_sym]
+          return nil if (@parent.attributes.include?(@name) && @parent.attributes[@name].nil? && @query_attrs.empty?) || foreign_key_value.blank?
 
           if @parent.attributes[@name].blank? || @query_attrs.any?
             path = begin
-              @klass.build_request_path(@parent.attributes.merge(@query_attrs.merge(@klass.primary_key => @parent.attributes[@opts[:foreign_key].to_sym])))
+              @klass.build_request_path(@parent.attributes.merge(@query_attrs.merge(@klass.primary_key => foreign_key_value)))
             rescue Her::Errors::PathError
               return nil
             end
