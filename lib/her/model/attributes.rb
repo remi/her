9ff0b3b0fd770b32e1998cb4190e51763212dd06
Zachary Welch <zwelch154@gmail.com>
2017-06-06 23:47:34 -0400

    move initialize_collection to class methods section

--- lib/her/model/attributes.rb
+++ lib/her/model/attributes.rb
@@ -30,22 +30,6 @@ module Her
         run_callbacks :initialize
       end
 
-      # Initialize a collection of resources
-      #
-      # @private
-      def self.initialize_collection(klass, parsed_data={})
-        collection_data = klass.extract_array(parsed_data).map do |item_data|
-          if item_data.kind_of?(klass)
-            resource = item_data
-          else
-            resource = klass.new(klass.parse(item_data))
-            resource.run_callbacks :find
-          end
-          resource
-        end
-        Her::Collection.new(collection_data, parsed_data[:metadata], parsed_data[:errors])
-      end
-
       # Use setter methods of model for each key / value pair in params
       # Return key / value pairs for which no setter method was defined on the model
       #
@@ -177,12 +161,28 @@ module Her
       end
 
       module ClassMethods
+        # Initialize a collection of resources
+        #
+        # @private
+        def initialize_collection(klass, parsed_data={})
+          collection_data = klass.extract_array(parsed_data).map do |item_data|
+            if item_data.kind_of?(klass)
+              resource = item_data
+            else
+              resource = klass.new(klass.parse(item_data))
+              resource.run_callbacks :find
+            end
+            resource
+          end
+          Her::Collection.new(collection_data, parsed_data[:metadata], parsed_data[:errors])
+        end
+
         # Initialize a collection of resources with raw data from an HTTP request
         #
         # @param [Array] parsed_data
         # @private
         def new_collection(parsed_data)
-          Her::Model::Attributes.initialize_collection(self, parsed_data)
+          initialize_collection(self, parsed_data)
         end
 
         # Initialize a new object with the "raw" parsed_data from the parsing middleware
