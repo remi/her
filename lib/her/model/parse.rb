13e10efe10b7bf9d6280f8f12729e9061842d969
James Le Cuirot <james.le-cuirot@yakara.com>
2017-12-20 16:33:13 +0000

    Serialize ActiveModel instances that do not use Her
    Faraday does not serialize everything thrown at it and passing such
    instances currently just results in useless ugly strings.
    
    ActiveModel::Serialization needs to be included for this to work.

--- lib/her/model/parse.rb
+++ lib/her/model/parse.rb
@@ -32,8 +32,18 @@ module Her
 
         # @private
         def to_params(attributes, changes={})
-          filtered_attributes = attributes.dup.symbolize_keys
+          filtered_attributes = attributes.each_with_object({}) do |(key, value), memo|
+            case value
+            when Her::Model
+            when ActiveModel::Serialization
+              value = value.serializable_hash.symbolize_keys
+            end
+
+            memo[key.to_sym] = value
+          end
+
           filtered_attributes.merge!(embeded_params(attributes))
+
           if her_api.options[:send_only_modified_attributes]
             filtered_attributes = changes.symbolize_keys.keys.inject({}) do |hash, attribute|
               hash[attribute] = filtered_attributes[attribute]
