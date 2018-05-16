18438889e3940e83b56b53bbf7a32a0f3e08f049
Sebastian Suttner <sebastian.suttner@gmail.com>
2018-05-16 15:27:09 -0300

    Rubocop fix: Layout/EmptyLineAfterMagicComment

--- lib/her/model/orm.rb
+++ lib/her/model/orm.rb
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 module Her
   module Model
     # This module adds ORM-like capabilities to the model
--- spec/model/orm_spec.rb
+++ spec/model/orm_spec.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+
 require File.join(File.dirname(__FILE__), "../spec_helper.rb")
 
 describe Her::Model::ORM do
