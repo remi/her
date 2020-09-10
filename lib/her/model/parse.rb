c88ae37ab315125968c72cccfa687916fa688121
Luke Gordon <lgordon@kalkomey.com>
2020-09-10 14:16:34 -0500

    Fix infinite loop when parsing nested associations

--- lib/her/model/parse.rb
+++ lib/her/model/parse.rb
@@ -61,15 +61,23 @@ module Her
 
         # @private
         def embeded_params(attributes)
-          associations.values.flatten.each_with_object({}) do |definition, hash|
-            value = case association = attributes[definition[:name]]
-                    when Her::Collection, Array
-                      association.map { |a| a.to_params }.reject(&:empty?)
-                    when Her::Model
-                      association.to_params
-                    end
-            hash[definition[:data_key]] = value if value.present?
+          associations.keys.each_with_object({}) do |key, hash|
+            associations[key].flatten.each do |definition|
+              next if hash[definition[:data_key]].present? && key == :belongs_to
+              embeded_association(attributes, definition, hash)
+            end
           end
+        end
+
+        # @private
+        def embeded_association(attributes, definition, hash)
+          value = case association = attributes[definition[:name]]
+                  when Her::Collection, Array
+                    association.map { |a| a.to_params }.reject(&:empty?)
+                  when Her::Model
+                    association.to_params
+                  end
+          hash[definition[:data_key]] = value if value.present?
         end
 
         # Return or change the value of `include_root_in_json`
